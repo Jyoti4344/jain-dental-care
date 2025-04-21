e066eabcd07d826bb6b19835f519cffcf6172b3f
Jyoti Goel <127315829+Jyoti4344@users.noreply.github.com>
2025-04-21 11:25:52 +0530

    Update ContactInfo.tsx

--- src/components/common/ContactInfo.tsx
+++ src/components/common/ContactInfo.tsx
@@ -11,8 +11,8 @@ const ContactInfo = () => {
           <MapPin size={24} className="text-tulip-dark-blue mr-4 mt-1 flex-shrink-0" />
           <div>
             <h4 className="font-semibold mb-1">Address</h4>
-            <p className="text-gray-700">#331, Near Dr. Chandrasen Wali Gali, Sihi Gate, Bania wara</p>
-            <p className="text-gray-700">Ballabhgarh, Faridabad, 121004</p>
+            <p className="text-gray-700">3236, 31, Vishnu Garden, Ward-30, Block-B, Sector-105</p>
+            <p className="text-gray-700">Gurugram, Haryana, 122006</p>
           </div>
         </div>
         
@@ -20,7 +20,7 @@ const ContactInfo = () => {
           <Phone size={24} className="text-tulip-dark-blue mr-4 mt-1 flex-shrink-0" />
           <div>
             <h4 className="font-semibold mb-1">Phone</h4>
-            <p className="text-gray-700">(91)-9711487748</p>
+            <p className="text-gray-700">(91)-8447945913</p>
           </div>
         </div>
         
